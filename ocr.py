d6499125d127a410464ca13d240df94a85ad3d42
Oscar Shiang <oscar0225@livemail.tw>
2021-01-07 23:25:25 +0800

    Fix UI corruption

--- ocr.py
+++ ocr.py
@@ -7,6 +7,8 @@ import  mxnet  as  mx
 
 import matplotlib.pyplot as plt
 
+ocr  = CnOcr()
+
 def text_reg(img_path, rois, class_ids):
     # img = cv2.imread(image_path)
     img = cv2.imread(img_path)
@@ -17,15 +19,17 @@ def text_reg(img_path, rois, class_ids):
 
     s_id = img[pos[0]:pos[2], (pos[1] - 30):pos[3]]
 
+    plt.figure()
+    plt.imshow(s_id)
+    plt.show()
+
     # print(f'size: {img.shape}')
 
     s = pytesseract.image_to_string(s_id)
     # print(s)
 
     word1 = "".join(re.findall("[a-zA-Z0-9]*", s))
-    print(word1[-9:])
-
-    ocr  = CnOcr()
+    print(f'ID: {word1[-9:]}')
 
     # [ 644  894  804 1287]
 
@@ -34,9 +38,10 @@ def text_reg(img_path, rois, class_ids):
 
     name = img[pos[0]:pos[2], (pos[1] - 30):pos[3]]
 
+    plt.figure()
     plt.imshow(name)
     plt.show()
 
     img = mx.nd.array(name)
     res  = ocr.ocr(name)
-    print( "姓名:" , res)
+    print(f"name: {res}")
